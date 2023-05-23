b68be73d9ddfc0cb20dc79268350583486c891a5
Admin <test1@gmail.com>
2023-05-23 20:26:11 +0300

    Completed chess

---
+++ src/Tests/Runnable/ChatServer.java
@@ -0,0 +1,42 @@
+package Tests.Runnable;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ChatServer {
+    ArrayList<Client> clients = new ArrayList<>();
+    ServerSocket serverSocket;
+    ChatServer() throws IOException{
+        serverSocket = new ServerSocket(1234);
+    }
+
+    void sendAll(String message) {
+        for (Client client : clients){
+            client.receive(message);
+        }
+    }
+    public void run() {
+        while(true) {
+            System.out.println("Waiting...");
+            // ждем клиента из сети
+            try {
+                Socket socket = serverSocket.accept();
+                System.out.println("Client connected!");
+                // создаем клиента на своей стороне
+                clients.add(new Client(socket, this));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        new ChatServer().run();
+    }
+}
